091f5277aeb5dc81d870d021873384b8e4199940
Christopher McDevitt <christophermcd0903@gmail.com>
2025-05-20 19:04:03 -0400

    rofi config

--- programs/gui.nix
+++ programs/gui.nix
@@ -15,6 +15,9 @@
   xdg.configFile."sway/sway-bar-script.sh".source = ../dotfiles/sway-bar-script.sh;
   xdg.configFile."sway/unova-map-background.png".source = ../dotfiles/unova-map-background.png;
 
+  xdg.configFile."rofi/config.rasi".source = ../dotfiles/config.rasi;
+  xdg.configFile."rofi/theme.rasi".source = ../dotfiles/theme.rasi;
+
   xdg.configFile."fish/config.fish".source = ../dotfiles/config.fish;
 
   programs.fish.shellAliases = {

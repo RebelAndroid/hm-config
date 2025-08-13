6e84a4b095528d1d373bc7bd1e41c220daca2ab9
Christopher McDevitt <christophermcd0903@gmail.com>
2025-08-13 15:12:23 -0400

    add sirula

--- programs/gui.nix
+++ programs/gui.nix
@@ -15,5 +15,8 @@
 
   xdg.configFile."alacritty/alacritty.toml".source = ../dotfiles/alacritty.toml;
 
+  xdg.configFile."sirula/config.toml".source = ../dotfiles/sirula/config.toml;
+  xdg.configFile."sirula/style.css".source = ../dotfiles/sirula/style.css;
+
   xdg.configFile."fish/config.fish".source = ../dotfiles/config.fish;
 }
